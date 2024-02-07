1f919cd7310df70eaf62910ae0b39c0ad1eb2f2b
i528376 <v.som.sekhar@sap.com>
2024-02-07 06:01:07 +0530

    google authentication, with out signout

--- Projects/netflix-clone/src/app/app-routing.module.ts
+++ Projects/netflix-clone/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadComponent: () => import('./login/login.component').then(a => a.LoginComponent) },
+  { path: 'browse', loadComponent: () => import('./browse/browse.component').then(a => a.BrowseComponent) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
