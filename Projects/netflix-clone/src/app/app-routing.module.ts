6d3ddf45e652b440c6088788af8047ba8b825ba5
i528376 <v.som.sekhar@sap.com>
2024-02-11 18:23:56 +0530

    core components added

--- Projects/netflix-clone/src/app/app-routing.module.ts
+++ Projects/netflix-clone/src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadComponent: () => import('./login/login.component').then(a => a.LoginComponent) },
-  { path: 'browse', loadComponent: () => import('./browse/browse.component').then(a => a.BrowseComponent) }
+  { path: '', loadComponent: () => import('./pages/login/login.component').then(a => a.LoginComponent) },
+  { path: 'browse', loadComponent: () => import('./pages/browse/browse.component').then(a => a.BrowseComponent) }
 ];
 
 @NgModule({
